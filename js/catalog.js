dd9034fa5955e452e10d808351ee7a8550baea79
Samuel-Njoroge <njorogekiragu8@gmail.com>
2022-12-10 22:48:57 +0300

    all

--- js/catalog.js
+++ js/catalog.js
@@ -16,7 +16,28 @@ function addProduct(name, price, description, imageURL) {
   const productCatalog = [];
   
   // Add some products to the catalog
-  addProduct("T-Shirt", 19.99, "A comfortable cotton t-shirt", "tshirt.jpg");
+  addProduct("T-Shirt", 19.99, "A comfortable cotton t-shirt", "/images/eucalyptus-1.jpg");
   addProduct("Baseball Hat", 14.99, "A stylish baseball hat", "baseballhat.jpg");
   addProduct("Sweater", 24.99, "A warm and cozy sweater", "sweater.jpg");
   
+addProduct();
+
+import { createBoard } from '@wixc3/react-board';
+import { ProductItem } from '../../../components/product-item/product-item';
+import itemPhoto from '../../../assets/matcha-tea.png';
+
+export default createBoard({
+    name: 'Matcha Tea ',
+    Board: () => (
+        <ProductItem
+            itemName="Matche Tea"
+            itemPhotoURL={itemPhoto}
+            itemPrice={9.99}
+            itemDescription="240 calories, 32g sugar, 7g fat."
+        />
+    ),
+    environmentProps: {
+        windowWidth: 600,
+        windowHeight: 800,
+    },
+});
